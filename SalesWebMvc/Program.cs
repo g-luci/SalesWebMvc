43ee7d800a5a2df25f56570f7f73c9d2c47b289f
Gabriel Lucas <gabriellucaslc10@gmail.com>
2025-03-25 16:43:32 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,21 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Conexao com o banco de dados MySQL
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext"))));
 
+//Registra a classe SeedingService no contêiner de injeção de dependêcia
 builder.Services.AddTransient<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+/*Criação de um escopo de injeção de dependência manualmente, garantindo que os serviços sejam corretamente
+resolvidos e descartados após o uso*/
 using (var scope = app.Services.CreateScope())
 {
+    //permite acessar os serviços registrados no contêiner de ID
     var services = scope.ServiceProvider;
     var seedingService = services.GetRequiredService<SeedingService>();
     seedingService.Seed();
